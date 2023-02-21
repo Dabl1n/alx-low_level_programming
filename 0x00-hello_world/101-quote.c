8fde089b8697545fe6f6b9402d7da4bb1598d638
Dabl1n <franklindablin@gmail.com>
2023-02-21 14:11:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio>
 #include <unistd.h>
-/* main - Entry point
+/** main - Entry point
  *  Return: 1 (Success)
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
