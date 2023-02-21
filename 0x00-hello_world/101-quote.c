3e5f45c0ed82f3e6503f7686055dced0c76164fc
Dabl1n <franklindablin@gmail.com>
2023-02-21 14:07:44 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio>
+#include <unistd.h>
+/* main - Entry point
+ *  Return: 1 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
