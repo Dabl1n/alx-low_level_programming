37ba583809f817507d8f6de33d409c07f85c2345
Dabl1n <franklindablin@gmail.com>
2023-02-27 18:48:17 -0800

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print strings
+ * @str:function parameter
+ * Return; 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 str[i] != '\0' : i++)
+	{
+		-putchar(str(i));
+	}
+	_putchar('\n');
+}
