afc6b0b8a647efba84f9a20401fa90204765399a
Dabl1n <franklindablin@gmail.com>
2023-03-05 16:00:05 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 /**
  * main - determines if a number is positive, negative or zero
+ *
  * Return : Always 0 (Success)
  */
 int main(void)
@@ -22,8 +23,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative\n, n);
+		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
